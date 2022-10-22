6e153bdb6ca512a80c0c7fa049802de36d914b13
Shane Stroud <shanemstroud@gmail.com>
2022-10-21 22:25:57 -0400

    initial commit
    built off of week fives code

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://shanemstroud:shane1234@firstcluster.953muqv.mongodb.net/media?retryWrites=true&w=majority"
